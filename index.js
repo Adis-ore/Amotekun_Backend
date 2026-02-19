13710b604bf661a9bfa60d5e59b71cd89c7c9206
Adis-ore <adisoreoluwa@gmail.com>
2026-02-19 10:26:32 +0100

    cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "https://amotekun-frontend.vercel.app",
   credentials: true,
 }));
 
